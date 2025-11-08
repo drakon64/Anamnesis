62d36fc33c72c0c7061ab3e9aa2d88a655d01a47
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-08 22:47:40 +0000

    Remove unneccessary service account member

--- tofu/module/iam.tf
+++ tofu/module/iam.tf
@@ -1,3 +1,9 @@
 resource "google_service_account" "anamnesis" {
   account_id = "anamnesis"
 }
+
+# resource "google_service_account_iam_member" "iam" {
+#   member             = google_service_account.anamnesis.member
+#   role               = "roles/iam.serviceAccountUser"
+#   service_account_id = google_service_account.anamnesis.id
+# }
