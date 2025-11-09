d234a05f0472a8b34ef907b585bc51d81acc638c
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-09 19:31:14 +0000

    Remove redundant service account IAM member

--- tofu/module/iam.tf
+++ tofu/module/iam.tf
@@ -1,9 +1,3 @@
 resource "google_service_account" "anamnesis" {
   account_id = "anamnesis"
 }
-
-# resource "google_service_account_iam_member" "iam" {
-#   member             = google_service_account.anamnesis.member
-#   role               = "roles/iam.serviceAccountUser"
-#   service_account_id = google_service_account.anamnesis.id
-# }
