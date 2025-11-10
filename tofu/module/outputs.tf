5be8271e6c0cf4c3df700b5005168aeac68ff757
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-10 17:05:55 +0000

    Add bucket and services to outputs

--- tofu/module/outputs.tf
+++ tofu/module/outputs.tf
@@ -1,6 +1,14 @@
+output "bucket" {
+  value = google_storage_bucket.bucket.name
+}
+
 output "load_balancer_ips" {
   value = {
     v4 = module.lb-http.external_ip
     v6 = module.lb-http.external_ipv6_address
   }
+}
+
+output "services" {
+  value = [for service in google_cloud_run_v2_service.service : service.id]
 }
--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -1,3 +1,11 @@
+output "bucket" {
+  value = module.anamnesis.bucket
+}
+
 output "load_balancer_ips" {
   value = module.anamnesis.load_balancer_ips
+}
+
+output "services" {
+  value = module.anamnesis.services
 }
