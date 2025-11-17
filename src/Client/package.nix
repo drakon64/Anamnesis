f8fdeeff080eb68ed9ef4cc276c1041ea9f94e05
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-17 17:24:22 +0000

    Add Client project

--- src/Client/package.nix
+++ src/Client/package.nix
@@ -3,14 +3,13 @@
   lib,
   buildDotnetModule,
   dotnetCorePackages,
-  dockerTools,
   ...
 }:
 let
   fs = lib.fileset;
 in
 buildDotnetModule (finalAttrs: {
-  pname = "anamnesis-dashboard";
+  pname = "anamnesis-client";
   version = builtins.readFile ../../version;
 
   src = fs.toSource {
@@ -18,7 +17,6 @@ buildDotnetModule (finalAttrs: {
 
     fileset = fs.difference (./.) (
       fs.unions [
-        ./appsettings.Development.json
         (lib.fileset.maybeMissing ./bin)
         (lib.fileset.maybeMissing ./config)
         (lib.fileset.maybeMissing ./obj)
@@ -30,26 +28,17 @@ buildDotnetModule (finalAttrs: {
     );
   };
 
-  projectFile = "Anamnesis.Dashboard.csproj";
+  projectFile = "Anamnesis.Client.csproj";
   nugetDeps = ./deps.json;
 
   dotnet-sdk = dotnetCorePackages.sdk_10_0;
-  dotnet-runtime = dotnetCorePackages.aspnetcore_10_0;
+  dotnet-runtime = dotnetCorePackages.runtime_10_0;
 
-  executables = [ "Anamnesis.Dashboard" ];
+  executables = [ "Anamnesis.Client" ];
 
   meta = {
     license = lib.licenses.eupl12;
-    mainProgram = "Anamnesis.Dashboard";
+    mainProgram = "Anamnesis.Client";
     maintainers = with lib.maintainers; [ drakon64 ];
-  };
-
-  passthru.docker = dockerTools.buildLayeredImage {
-    name = "anamnesis-dashboard";
-    tag = "latest";
-
-    config.Entrypoint = [ (lib.getExe finalAttrs.finalPackage) ];
-
-    contents = [ dockerTools.caCertificates ];
   };
 })
