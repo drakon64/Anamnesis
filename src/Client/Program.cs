81ab344b8e0fec143a285a07ffdf630584b1b5a0
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-17 23:33:42 +0000

    Client options

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+var directoryArgument = new Argument<DirectoryInfo>("directory")
+{
+    Description = "The directory to upload as a module"
+};
+
+var latestOption = new Option<bool>("--no-latest")
+{
+    Description = "Don't mark the uploaded module as the latest"
+};
+
+var rootCommand = new RootCommand("Anamnesis registry client");
+rootCommand.Arguments.Add(directoryArgument);
+rootCommand.Options.Add(latestOption);
+
+return rootCommand.Parse(args).Invoke();
