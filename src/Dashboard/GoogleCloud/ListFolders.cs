eb8c6b02f5e40b442e46512b5462387ec451661e
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-17 21:00:20 +0000

    Remove unused method override

--- src/Dashboard/GoogleCloud/ListFolders.cs
+++ src/Dashboard/GoogleCloud/ListFolders.cs
@@ -27,34 +27,6 @@ internal static partial class GoogleCloud
 
         return (from folder in folders select folder.Name.TrimEnd('/')).ToArray();
     }
-
-    internal static async Task<string[]> ListFolders(string bucket, string prefix)
-    {
-        var queryPrefix = prefix.EndsWith('/') ? prefix : $"{prefix}/";
-
-        using var request = new HttpRequestMessage();
-        request.Headers.Add("Authorization", await GetAccessToken());
-        request.RequestUri = new Uri(
-            $"https://storage.googleapis.com/storage/v1/b/{bucket}/folders?delimiter=/&prefix={queryPrefix}"
-        );
-
-        using var response = await HttpClient.SendAsync(request);
-
-        if (!response.IsSuccessStatusCode)
-            throw new Exception(await response.Content.ReadAsStringAsync());
-
-        return (
-            from folder in (
-                (
-                    await response.Content.ReadFromJsonAsync(
-                        SourceGenerationContext.Default.ListFoldersResponse
-                    )
-                )!
-            ).Items
-            where folder.Name != queryPrefix
-            select folder.Name.Replace(prefix, "").Trim('/')
-        ).ToArray();
-    }
 }
 
 internal sealed class ListFoldersResponse
