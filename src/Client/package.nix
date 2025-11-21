281fd34cfee129295839f254faf244344a570636
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-21 12:58:32 +0000

    Revert "Remove Client"
    This reverts commit 4537628889f7b18accb2eec020a62c74ee92ffb0.

---
+++ src/Client/package.nix
@@ -0,0 +1,44 @@
+{
+  pkgs,
+  lib,
+  buildDotnetModule,
+  dotnetCorePackages,
+  ...
+}:
+let
+  fs = lib.fileset;
+in
+buildDotnetModule (finalAttrs: {
+  pname = "anamnesis-client";
+  version = builtins.readFile ../../version;
+
+  src = fs.toSource {
+    root = ./.;
+
+    fileset = fs.difference (./.) (
+      fs.unions [
+        (lib.fileset.maybeMissing ./bin)
+        (lib.fileset.maybeMissing ./config)
+        (lib.fileset.maybeMissing ./obj)
+        ./.gitignore
+
+        ./package.nix
+        (lib.fileset.maybeMissing ./deps.json)
+      ]
+    );
+  };
+
+  projectFile = "Anamnesis.Client.csproj";
+  nugetDeps = ./deps.json;
+
+  dotnet-sdk = dotnetCorePackages.sdk_10_0;
+  dotnet-runtime = dotnetCorePackages.runtime_10_0;
+
+  executables = [ "Anamnesis.Client" ];
+
+  meta = {
+    license = lib.licenses.eupl12;
+    mainProgram = "Anamnesis.Client";
+    maintainers = with lib.maintainers; [ drakon64 ];
+  };
+})
